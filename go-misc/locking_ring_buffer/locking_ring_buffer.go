9e34485ad312fac1c1c7a1424e10b752085f1333
Jean de Klerk <jadekler@gmail.com>
2016-05-15 19:43:12 -0600

    Start work on locking ring buffer

---
+++ go-misc/locking_ring_buffer/locking_ring_buffer.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+    buffer := newRingBuffer(3)
+
+    buffer.print()
+
+    buffer.queueWord("hello")
+    buffer.queueWord("world")
+    buffer.queueWord("this")
+    buffer.queueWord("is")
+    buffer.queueWord("jean")
+
+    buffer.print()
+}
+
+type ringBuffer struct {
+    buffer         []string
+    currentPointer int
+}
+
+func newRingBuffer(size int) ringBuffer {
+    return ringBuffer{
+        buffer: make([]string, size),
+        currentPointer: 0,
+    }
+}
+
+func (buffer ringBuffer) print() {
+    output := ""
+
+    for key, value := range buffer.buffer {
+        output += fmt.Sprintf("[%d, '%s'] ", key, value)
+    }
+
+    fmt.Println(output)
+}
+
+func (buffer *ringBuffer) queueWord(word string) {
+    buffer.buffer[buffer.currentPointer] = word
+
+    if buffer.currentPointer + 1 == len(buffer.buffer) {
+        buffer.currentPointer = 0
+    } else {
+        buffer.currentPointer++
+    }
+}
